4bed651cd9415fdd4fd0e72cde2f1d6dcf03c683
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 14:01:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -50,8 +50,7 @@ def sum_array(arr)
 end
 
 def add_s(arr)
-  arr.each_with_index.collect{|element, index| index != 1 element << "s" }
-end
+  arr.each_with_index.collect{|element, index| if (index != 1) then element << "s" }
 end
 
 
